d56664f6b99156e5baf346b89891db5163f9e0f8
LiuYi <1354548951@qq.com>
2023-05-06 00:29:59 +0800

    工具类更新，安卓版本最低要求改至Android 11

--- app/src/main/java/org/liuyi/mifreeformx/activity/page/MyBasePage.kt
+++ app/src/main/java/org/liuyi/mifreeformx/activity/page/MyBasePage.kt
@@ -2,6 +2,7 @@ package org.liuyi.mifreeformx.activity.page
 
 import androidx.annotation.StringRes
 import cn.fkj233.ui.activity.data.BasePage
+import cn.fkj233.ui.activity.view.SpinnerV
 import cn.fkj233.ui.activity.view.SwitchV
 import com.highcapable.yukihookapi.hook.factory.prefs
 import com.highcapable.yukihookapi.hook.xposed.prefs.data.PrefsData
@@ -17,6 +18,13 @@ abstract class MyBasePage : BasePage() {
     internal fun createSwitchV(prefsData: PrefsData<Boolean>) =
         SwitchV(prefsData.key, activity.prefs().get(prefsData)) {
             activity.prefs().edit { put(prefsData, it) }
+        }
+
+    internal fun createSpinnerV(prefsData: PrefsData<Int>, textList: List<String>) =
+        SpinnerV(currentValue = textList[activity.prefs().get(prefsData)]) {
+            textList.forEachIndexed { index, text ->
+                add(text) { activity.prefs().edit { put(prefsData, index) } }
+            }
         }
 
     internal fun openFunText(@StringRes textId: Int) = getString(R.string.open) + getString(textId)
--- app/src/main/java/org/liuyi/mifreeformx/utils/ReflectUtils.kt
+++ app/src/main/java/org/liuyi/mifreeformx/utils/ReflectUtils.kt
@@ -1,6 +1,9 @@
 package org.liuyi.mifreeformx.utils
 
+import com.highcapable.yukihookapi.hook.core.finder.members.FieldFinder
+import com.highcapable.yukihookapi.hook.core.finder.members.MethodFinder
 import com.highcapable.yukihookapi.hook.factory.field
+import com.highcapable.yukihookapi.hook.factory.method
 
 /**
  * @Author: Liuyi
@@ -10,6 +13,21 @@ import com.highcapable.yukihookapi.hook.factory.field
 class ReflectUtils {
 }
 
-fun Any.getFieldValueOrNull(fieldName: String) = javaClass.field { name(fieldName) }.get(this).any()
-fun Any.setFieldValue(fieldName: String, value: Any?) =
+internal fun Any.getFieldValueOrNull(fieldName: String) =
+    javaClass.field { name(fieldName) }.get(this).any()
+
+internal fun Any.getFieldValueByName(fieldName: String) =
+    javaClass.field { name(fieldName) }.get(this).any()!!
+
+
+internal fun Any.getFieldValue(initiate: FieldFinder.() -> Unit) =
+    javaClass.field(initiate).get(this).any()
+
+internal fun Any.setFieldValue(fieldName: String, value: Any?) =
     javaClass.field { name(fieldName) }.get(this).set(value)
+
+internal fun Any.callMethodByName(methodName: String, vararg args: Any?) =
+    javaClass.method { name(methodName) }.get(this).call(*args)
+
+internal fun Any.callMethod(vararg args: Any?, initiate: MethodFinder.() -> Unit) =
+    javaClass.method(initiate).get(this).call(*args)
