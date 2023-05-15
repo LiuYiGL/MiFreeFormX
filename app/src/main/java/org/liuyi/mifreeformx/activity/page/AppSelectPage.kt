55bc1a337632108acc99c0562fa96d948f297185
LiuYi <1354548951@qq.com>
2023-05-16 02:18:50 +0800

    应用选择页面添加搜索和筛选功能

--- app/src/main/java/org/liuyi/mifreeformx/activity/page/AppSelectPage.kt
+++ app/src/main/java/org/liuyi/mifreeformx/activity/page/AppSelectPage.kt
@@ -3,6 +3,7 @@ package org.liuyi.mifreeformx.activity.page
 import android.annotation.SuppressLint
 import cn.fkj233.ui.activity.annotation.BMPage
 import cn.fkj233.ui.activity.fragment.MIUIFragment
+import cn.fkj233.ui.activity.view.SpinnerV
 import com.blankj.utilcode.util.AppUtils
 import com.highcapable.yukihookapi.hook.factory.prefs
 import com.highcapable.yukihookapi.hook.log.loggerD
@@ -45,6 +46,38 @@ class AppSelectPage : MyBasePage() {
 
 
     override fun onCreate() {
+        val allAppInfoListBak = mAppSelectAdapter.mAppInfoList
+        var filterAppInfoListBak = mAppSelectAdapter.mAppInfoList
+        EditTextWithSpinner(
+            "", "输入软件名、包名", editTextWeight = 1.5f,
+            spinnerV = SpinnerV("全部") {
+                add("全部") {
+                    mAppSelectAdapter.mAppInfoList = allAppInfoListBak.sort(mAppSelectAdapter.selectedSet)
+                    filterAppInfoListBak = mAppSelectAdapter.mAppInfoList
+                    mAppSelectAdapter.notifyDataSetChanged()
+                }
+                add("用户应用") {
+                    mAppSelectAdapter.mAppInfoList =
+                        allAppInfoListBak.filter { appInfo -> !appInfo.isSystem }.toMutableList()
+                            .sort(mAppSelectAdapter.selectedSet)
+                    filterAppInfoListBak = mAppSelectAdapter.mAppInfoList
+                    mAppSelectAdapter.notifyDataSetChanged()
+                }
+                add("系统应用") {
+                    mAppSelectAdapter.mAppInfoList =
+                        allAppInfoListBak.filter { appInfo -> appInfo.isSystem }.toMutableList()
+                            .sort(mAppSelectAdapter.selectedSet)
+                    filterAppInfoListBak = mAppSelectAdapter.mAppInfoList
+                    mAppSelectAdapter.notifyDataSetChanged()
+                }
+            }
+        ) {
+            mAppSelectAdapter.mAppInfoList =
+                if (it.isBlank()) filterAppInfoListBak
+                else filterAppInfoListBak.filterByInputStr(it)
+            mAppSelectAdapter.mAppInfoList.sort(mAppSelectAdapter.selectedSet)
+            mAppSelectAdapter.notifyDataSetChanged()
+        }
         List {
             adapter = mAppSelectAdapter
         }
@@ -57,17 +90,7 @@ class AppSelectPage : MyBasePage() {
             val current = currentBlackList?.getAll(activity.prefs())
 
             mAppInfoList = current?.let {
-                allAppInfo.toMutableList().apply {
-                    sortWith { a1, a2 ->
-                        if (current.contains(a1.packageName) && current.contains(a2.packageName)) {
-                            PinyinUtils.ccs2Pinyin(a1.name).compareTo(PinyinUtils.ccs2Pinyin(a2.name))
-                        } else if (current.contains(a1.packageName)) {
-                            -1
-                        } else if (current.contains(a2.packageName)) {
-                            1
-                        } else PinyinUtils.ccs2Pinyin(a1.name).compareTo(PinyinUtils.ccs2Pinyin(a2.name))
-                    }
-                }
+                allAppInfo.toMutableList().sort(current)
             } ?: mutableListOf()
 
             disEnabledSet = currentBlackList?.forceList ?: setOf()
@@ -83,5 +106,25 @@ class AppSelectPage : MyBasePage() {
         currentBlackList = null
         fragment.initData()
         fragment.closeLoading()
+    }
+
+    fun MutableList<AppUtils.AppInfo>.sort(selected: Set<String>): MutableList<AppUtils.AppInfo> {
+        sortWith { a1, a2 ->
+            if (selected.contains(a1.packageName) && selected.contains(a2.packageName)) {
+                PinyinUtils.ccs2Pinyin(a1.name).compareTo(PinyinUtils.ccs2Pinyin(a2.name))
+            } else if (selected.contains(a1.packageName)) {
+                -1
+            } else if (selected.contains(a2.packageName)) {
+                1
+            } else PinyinUtils.ccs2Pinyin(a1.name).compareTo(PinyinUtils.ccs2Pinyin(a2.name))
+        }
+        return this
+    }
+
+    fun MutableList<AppUtils.AppInfo>.filterByInputStr(str: String): MutableList<AppUtils.AppInfo> {
+        return filter {
+            it.name.contains(str, true)
+                    || it.packageName.contains(str, true)
+        }.toMutableList()
     }
 }
